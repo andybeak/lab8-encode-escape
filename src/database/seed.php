8d9b58483b39084865c7258099857c4d71f4308c
Andrew Beak <andy.beak@gmail.com>
2019-12-19 11:20:48 +0000

    Add seed.php

--- src/database/seed.php
+++ src/database/seed.php
@@ -4,6 +4,6 @@ $host = 'mysql';
 $user = '53f178fe';
 $password = '53f178fe-e9db-490f-8c30-409b793f6873';
 
-$command = "mysql –h $host -u $user -p{$password} injection_demo < seed.sql";
+$command = "mysql -h $host -u $user -p{$password} injection_demo < seed.sql";
 
 shell_exec($command);
