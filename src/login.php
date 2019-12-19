65380e9ecbe3f0edeaa66e1284fb48b7c1fe079d
Andrew Beak <andy.beak@gmail.com>
2019-12-19 11:34:52 +0000

    Add submit button

--- src/login.php
+++ src/login.php
@@ -13,6 +13,8 @@
         Username: <input type="text" name="username" />
         <br />
         Password: <input type="password" name="password" />
+        <br />
+        <input type="submit" value="Login" />
     </form>
 
 </body>
