5a6be76813cf8a1bfc801c358e7c19ec6601d9f4
Andrew Beak <andy.beak@gmail.com>
2019-12-19 16:59:48 +0000

    Mitigate shell parameter injection

--- src/database/seed.php
+++ src/database/seed.php
@@ -1,9 +1,12 @@
 <?php
 
-$host = 'mysql';
-$user = '53f178fe';
-$password = '53f178fe-e9db-490f-8c30-409b793f6873';
+// $host = 'mysql';
+$host = escapeshellarg('; wget https://www.php.net/images/logos/php-logo.svg; #');
+$user = escapeshellarg('53f178fe');
+$password = escapeshellarg('53f178fe-e9db-490f-8c30-409b793f6873');
 
 $command = "mysql -h $host -u $user -p{$password} injection_demo < seed.sql";
+
+echo $command . PHP_EOL . PHP_EOL;
 
 shell_exec($command);
