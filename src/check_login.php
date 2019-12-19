48d56e56551b0c2745f1fbf3f1cb2741a5bd36a4
Andrew Beak <andy.beak@gmail.com>
2019-12-19 09:33:11 +0000

    Change to use PDO

--- src/check_login.php
+++ src/check_login.php
@@ -1,10 +1,15 @@
 <?php
 
-$mysqli = new mysqli('mysql', '53f178fe', '53f178fe-e9db-490f-8c30-409b793f6873', 'injection_demo');
+$dsn = 'mysql:dbname=injection_demo;host=mysql';
+$user = '53f178fe';
+$password = '53f178fe-e9db-490f-8c30-409b793f6873';
 
-if ($mysqli->connect_error) {
-    die('Connect Error (' . $mysqli->connect_errno . ') '
-            . $mysqli->connect_error);
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
 }
 
-$result = $mysqli->query("SELECT * FROM users WHERE username=" . $_POST['username'] . '" AND password="' . $_POST['password'] . '"');
+$sql = 'SELECT * FROM users WHERE username="' . $_POST['username'] .'" AND password="' . $_POST['password'] .'"';
+
+$result = $dbh->query($sql);
