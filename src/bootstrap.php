af4eb0c909154d1a530b31728b4b19958cdeb538
Andrew Beak <andy.beak@gmail.com>
2019-12-19 12:25:27 +0000

    Change location of log

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,10 +5,13 @@ require('vendor/autoload.php');
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-// Create the logger
-$logger = new Logger('demo');
-// Now add some handlers
-$logger->pushHandler(new StreamHandler(
-    __DIR__ . '/../log/' . date('Y-m-d') . '.log',
-    Logger::DEBUG)
-);
+try {
+    $logger = new Logger('demo');
+    $logger->pushHandler(new StreamHandler(
+            __DIR__ . '/log/' . date('Y-m-d') . '.log',
+            Logger::DEBUG)
+    );
+} catch (Throwable $e) {
+    echo "Could not instantiate logger because [{$e->getMessage()}]";
+}
+
